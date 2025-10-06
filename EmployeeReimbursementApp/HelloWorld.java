17cd698a1dbc551a3b74350a4b3e2a3dd8b9fe54
Karl <g_cera@yahoo.com>
2025-10-06 02:07:53 -0300

    Testing fix

--- EmployeeReimbursementApp/HelloWorld.java
+++ EmployeeReimbursementApp/HelloWorld.java
@@ -1,10 +1,10 @@
-import java.*
-public static int toPow(int num, int power){
-    return num * power;
-}
+import java.*;
+
 
 public class HelloWorld {
-
+    public static int toPow(int num, int power){
+        return num * power;
+    }
     public static void main(String[] args) {
         int num = 9;
         int pow = 3;
