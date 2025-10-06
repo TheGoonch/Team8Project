267641f47d62a86cfa55ca21b888fcc4bd9ce697
Karl <g_cera@yahoo.com>
2025-10-06 02:06:09 -0300

    Testing

---
+++ EmployeeReimbursementApp/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.*
+public static int toPow(int num, int power){
+    return num * power;
+}
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int num = 9;
+        int pow = 3;
+        System.out.println(toPow(num, pow);
+    }
+
+}
