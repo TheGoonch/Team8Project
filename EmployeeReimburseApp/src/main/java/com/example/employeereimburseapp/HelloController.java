4a00e938a29838b05372d7d847cc875d14f58c94
Karl <g_cera@yahoo.com>
2025-10-29 23:07:22 -0300

    Adding Log in page

--- EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/HelloController.java
+++ EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.employeereimburseapp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void takeMeToLogin(ActionEvent event) throws IOException {
+        System.out.println("Hello World");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage newStage = new Stage();
+        newStage.setTitle("Login Window");
+        newStage.setScene(scene);
+        newStage.show();
     }
 }
