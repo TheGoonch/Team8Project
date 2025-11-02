4805e88c6eb7ec3712ac6409e8367ea1d41cfadb
Karl <g_cera@yahoo.com>
2025-11-01 23:50:20 -0300

    Added login-page.fxml and modified hello-view.fxml, and adding code that wold connect button from hello-view to login-page

--- EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/HelloController.java
+++ EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.employeereimburseapp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void takeToLogInPage(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage newStage = new Stage();
+        newStage.setTitle("Login Window");
+        newStage.setScene(scene);
+        newStage.show();
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
     }
 }
---
+++ EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/LoginPageController.java
@@ -0,0 +1,12 @@
+package com.example.employeereimburseapp;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginPageController {
+
+}
