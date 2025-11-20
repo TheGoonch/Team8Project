266d7e7bc71b9b3e8ad9b32fd28aee46a355ad6e
Karl <g_cera@yahoo.com>
2025-11-20 15:01:43 -0400

    Saving work on addcancelreqtest

--- EmployeeReimburseApp/src/test/java/testpackage/AddCancelRequestTest.java
+++ EmployeeReimburseApp/src/test/java/testpackage/AddCancelRequestTest.java
@@ -1,4 +1,38 @@
 package testpackage;
 
+import com.example.employeereimburseapp.CentralDatabase;
+import com.example.employeereimburseapp.EmployeeDashController;
+import com.example.employeereimburseapp.LoginPageController;
+import com.example.employeereimburseapp.UserSession;
+import javafx.application.Platform;
+import javafx.scene.control.TextField;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class AddCancelRequestTest {
+
+    private EmployeeDashController controller;
+    private static LoginPageController sessionCreator;
+
+    @BeforeAll
+    static void initJavaFx() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Platform.startup(() -> {
+            Connection con = CentralDatabase.getConnection();
+            try {
+                sessionCreator.createSession(con);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            latch.countDown();
+        });
+        latch.await(5, TimeUnit.SECONDS);
+    }
+
+
 }
