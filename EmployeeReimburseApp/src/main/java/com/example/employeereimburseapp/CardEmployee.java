777d2c85b0feabd9e44b8da15470f3a306fa9ea2
Karl <g_cera@yahoo.com>
2025-11-20 16:04:21 -0400

    Added checkStatus to both methods in cardemployee so you won't be able to edit or cancel approved or rejected requests

--- EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/CardEmployee.java
+++ EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/CardEmployee.java
@@ -3,7 +3,6 @@ package com.example.employeereimburseapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -17,6 +16,14 @@ public class CardEmployee extends RequestCard {
 
     @FXML
     public void moreInfo(ActionEvent event) throws IOException {
+        try(Connection con = CentralDatabase.getConnection()){
+            if(checkStatus(con)){
+                System.out.println("Request cannot be viewed");
+                return;
+            }
+        }catch(SQLException e){
+            System.out.println("Error in moreInfo\n" + e.getMessage() + e.getSQLState());
+        }
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("requestInfo-page.fxml"));
         Scene scene = new Scene(loader.load());
@@ -36,23 +43,39 @@ public class CardEmployee extends RequestCard {
     @FXML
     public void cancelRequest(){
         try(Connection con = CentralDatabase.getConnection()){
-            PreparedStatement ps = con.prepareStatement("SELECT \"status\" FROM \"Request\" WHERE \"req_id\" = ?");
-            ps.setInt(1, getRequestID());
-            ResultSet rs = ps.executeQuery();
-            rs.next();
-            String status = rs.getString("status");
-            if(status.equals("Rejected") || status.equals("Approved")){
+
+            if(checkStatus(con)){
                 System.out.println("Request cannot be cancelled");
                 return;
             }
-            ps = con.prepareStatement("DELETE FROM \"Request\" WHERE \"req_id\" = ?");
+            PreparedStatement ps = con.prepareStatement("DELETE FROM \"Request\" WHERE \"req_id\" = ?");
             ps.setInt(1, getRequestID());
             ps.executeUpdate();
             System.out.println("Request deleted");
             removeScene();
         }catch(SQLException e){
-            System.out.println("Error in getting database connection in Request Card\n" + e.getMessage());
+            System.out.println("Error in cancelling request card\n" + e.getMessage() + e.getSQLState());
         }
     }
+
+    public boolean checkStatus(Connection con){
+        try(PreparedStatement ps = con.prepareStatement("SELECT \"status\" FROM \"Request\" WHERE \"req_id\" = ?");){
+            ps.setInt(1, getRequestID());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            String status = rs.getString("status");
+            if (status.equals("Rejected") || status.equals("Approved")) {
+                return true;
+            }else if(status.equals("Pending")){
+                return false;
+            }
+        }catch (SQLException e){
+            System.out.println("Check Status failed\n" + e.getMessage());
+            return true;
+        }
+        return false;
+    }
+
+
 
 }
