427b6b6bda142596612243eff4b2664cfbbee113
Karl <g_cera@yahoo.com>
2025-12-04 14:45:16 -0400

    Styling and CSS added to log in and about me

--- EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/HelloController.java
+++ EmployeeReimburseApp/src/main/java/com/example/employeereimburseapp/HelloController.java
@@ -2,7 +2,11 @@ package com.example.employeereimburseapp;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloController {
 
